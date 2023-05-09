15f7f60a182a4e0987abcd89310a76ba8b3ea27f
Bobby Lee <ldg@dgit.co>
2023-05-09 20:15:03 +0900

    type resolver

--- tweet/server.js
+++ tweet/server.js
@@ -17,6 +17,7 @@ const typeDefs = gql`
 		username: String!
 		firstName: String
 		lastName: String
+		fullName: String
 	}
 	type Tweet {
 		id: ID!
@@ -24,6 +25,7 @@ const typeDefs = gql`
 		author: User
 	}
 	type Query {
+		allUsers: [User!]!
 		allTweets: [Tweet]!
 		tweet(id: ID!): Tweet
 	}
@@ -33,6 +35,19 @@ const typeDefs = gql`
 	}
 `;
 
+let users = [
+	{
+		id: "1",
+		firstName: "Bobby",
+		lastName: "Lee",
+	},
+	{
+		id: "2",
+		firstName: "Elon",
+		lastName: "Mask",
+	},
+];
+
 const resolvers = {
 	Query: {
 		allTweets() {
@@ -40,6 +55,9 @@ const resolvers = {
 		},
 		tweet(root, { id }) {
 			return tweets.findIndex((tweet) => tweet.id === id);
+		},
+		allUsers() {
+			return users;
 		},
 	},
 	Mutation: {
@@ -56,6 +74,11 @@ const resolvers = {
 			if (!tweet) return false;
 			tweets = tweets.filter((tweet) => tweet.id !== id);
 			return true;
+		},
+	},
+	User: {
+		fullName({ firstName, lastName }) {
+			return `${firstName} ${lastName}`;
 		},
 	},
 };
