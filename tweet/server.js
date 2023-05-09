8e05e1ba2b3c28e71aeae9aec5c31c6b3bbe7808
Bobby Lee <ldg@dgit.co>
2023-05-09 17:12:59 +0900

    resolver

--- tweet/server.js
+++ tweet/server.js
@@ -1,25 +1,66 @@
 import { ApolloServer, gql } from "apollo-server";
 
+let tweets = [
+	{
+		id: "1",
+		text: "first",
+	},
+	{
+		id: "2",
+		text: "second",
+	},
+];
+
 const typeDefs = gql`
 	type User {
-		id: ID
-		username: String
+		id: ID!
+		username: String!
+		firstName: String
+		lastName: String
 	}
 	type Tweet {
-		id: ID
-		text: String
+		id: ID!
+		text: String!
 		author: User
 	}
 	type Query {
-		allTweets: [Tweet]
-		tweet(id: ID): Tweet
+		allTweets: [Tweet]!
+		tweet(id: ID!): Tweet
 	}
 	type Mutation {
-		postTweet(text: String, userId: ID): Tweet
+		postTweet(text: String!, userId: ID!): Tweet!
+		deleteTweet(id: ID!): Boolean!
 	}
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+	Query: {
+		allTweets() {
+			return tweets;
+		},
+		tweet(root, { id }) {
+			return tweets.findIndex((tweet) => tweet.id === id);
+		},
+	},
+	Mutation: {
+		postTweet(_, { text, userId }) {
+			const newTweet = {
+				id: tweets.length + 1,
+				text,
+			};
+			tweets.push(newTweet);
+			return newTweet;
+		},
+		deleteTweet(_, { id }) {
+			const tweet = tweets.find((tweet) => tweet.id === id);
+			if (!tweet) return false;
+			tweets = tweets.filter((tweet) => tweet.id !== id);
+			return true;
+		},
+	},
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
 	console.log(`Running on ${url}`);
