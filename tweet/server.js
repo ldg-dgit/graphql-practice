63b5d811eb11005d633b41e7c471b35b96772826
Bobby Lee <ldg@dgit.co>
2023-05-09 10:43:03 +0900

    scalar and root types

---
+++ tweet/server.js
@@ -0,0 +1,23 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+	type User {
+		id: ID
+		username: String
+	}
+	type Tweet {
+		id: ID
+		text: String
+		author: User
+	}
+	type Query {
+		allTweets: [Tweet]
+		tweet(id: ID): Tweet
+	}
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+	console.log(`Running on ${url}`);
+});
