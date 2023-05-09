a5ec7cefd4d38d8231473e19488b9b746343cf77
Bobby Lee <ldg@dgit.co>
2023-05-09 10:57:54 +0900

    mutation type

--- tweet/server.js
+++ tweet/server.js
@@ -14,6 +14,9 @@ const typeDefs = gql`
 		allTweets: [Tweet]
 		tweet(id: ID): Tweet
 	}
+	type Mutation {
+		postTweet(text: String, userId: ID): Tweet
+	}
 `;
 
 const server = new ApolloServer({ typeDefs });
